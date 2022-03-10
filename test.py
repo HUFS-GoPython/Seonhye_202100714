3ddbe74b7b4bdd6a6a70a478ff0e9e33b118e90f
dufteo <100920482+dufteo@users.noreply.github.com>
2022-03-10 22:50:02 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,63 +1,7 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "91a8757a-7592-4627-87e6-4044dd9400c7",
-   "metadata": {
-    "tags": []
-   },
-   "outputs": [],
-   "source": [
-    "a='helloworld!'"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "id": "0dcc2acb-4a2c-4cb1-aa5e-99faac7da69a",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "helloworld!\n"
-     ]
-    }
-   ],
-   "source": [
-    "print(a)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "adbaf7a4-ae84-4be6-a89a-3ea7c3488513",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+Python 3.8.2 (v3.8.2:7b3ab5921f, Feb 24 2020, 17:52:18) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> test='helloworld?'
+>>> print(test)
+helloworld?
+>>> 
